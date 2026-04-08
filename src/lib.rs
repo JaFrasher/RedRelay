42c588d300f4199608bb458a36495224f6d2052b
joet <obviousspice@gmail.com>
2026-04-08 05:37:51 -0500

    Upgrade to red4ext-rs v0.9.1, new Plugin trait API

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,23 @@
-use red4ext_rs::prelude::*;
+use red4ext_rs::{export_plugin_symbols, exports, global, wcstr, Exportable, GlobalExport, Plugin, SemVer, U16CStr};
 
-define_plugin! {
-    name: "RedRelay",
-    author: "JaFrasher",
-    version: 0:1:0,
-    on_register: {
-        register_function!("ScheduleRequest", schedule_request);
-        register_function!("GetResponse", get_response);
-        register_function!("GetStatus", get_status);
-        register_function!("CancelRequest", cancel_request);
+pub struct RedRelay;
+
+impl Plugin for RedRelay {
+    const AUTHOR: &'static U16CStr = wcstr!("JaFrasher");
+    const NAME: &'static U16CStr = wcstr!("RedRelay");
+    const VERSION: SemVer = SemVer::new(0, 1, 0);
+
+    fn exports() -> impl Exportable {
+        exports![
+            GlobalExport(global!(c"ScheduleRequest", schedule_request)),
+            GlobalExport(global!(c"GetResponse", get_response)),
+            GlobalExport(global!(c"GetStatus", get_status)),
+            GlobalExport(global!(c"CancelRequest", cancel_request)),
+        ]
     }
 }
+
+export_plugin_symbols!(RedRelay);
 
 fn schedule_request(_request_id: String, _method: String, _url: String, _body: String) {}
 
